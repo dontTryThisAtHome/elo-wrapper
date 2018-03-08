d84fd72c4454da07f1c4187e21d00cda7e4ba3f5
Constantine <derkach@benefitty.ru>
2018-03-08 12:43:16 +0300

    edit config

--- config/config.php
+++ config/config.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
- * EloWrapper configuration file.
+ * The EloWrapper configuration file.
  *
  * Register your own wrappers according the Test wrapper schema below.
  *
- * To create wrapper use 'php artisan wrapper::generate {name}' command;
+ * To create a wrapper use 'php artisan wrapper::generate {name}' command;
  *
- * To create EloWrapper suitable Eloquent model use 'php artisan wrapper::model {name} command'
- * or extend the existing model from EloWrapper\Models\Model class;
+ * To create the EloWrapper suitable Eloquent model use 'php artisan wrapper::model {name}' command
+ * or extend the existing model with EloWrapper\Models\Model class;
  */
 return [
 
-    App\User::class => [ //Specify the model class
+    App\Test::class => [ //Specify the model class
         'test' => EloWrapper\Wrappers\Test::class //Specify the method name and the wrapper class name
     ],
     //Put your wrapper here
